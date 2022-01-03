5c64f9b053da8f205ac1565e0e6fd7f715e69a2b
Edmondas Girkantas <edmondas@girkantas.lt>
2022-01-03 22:02:32 +0200

    Updated script for incrementing copyright dates.

--- update_copyright.pl
+++ update_copyright.pl
@@ -9,20 +9,19 @@ my @date = localtime(time);
 my $curr_year = $date[5] + 1900;
 my $last_year = $curr_year - 1;
 
-$curr_year = 2012;
-$last_year = 2011;
+$curr_year = 2022;
+$last_year = 2017;
 
 sub process_file {
 	my $fn = $File::Find::name;
 	
-	return if ($fn =~ /\.svn/);
-	return if (-d $_);
+	return if ($fn =~ /\.git/);
 
 	my $found = 0;
 
 	open(FH, "$_") or die $!." <$fn>\n";
 	while (my $line = <FH>) {
-		if ($line =~ /Copyright 2010-$last_year  Poweradmin/) {
+		if ($line =~ /2010-$last_year  Poweradmin/) {
 			$found = 1;
 			last;
 		}
@@ -34,10 +33,9 @@ sub process_file {
 		open(OUT, "> $_.new") or die $!." <$fn.new>\n";
 
 		while (my $line = <IN>) {
-			if ($line =~ /Copyright 2010-$last_year  Poweradmin/) {
+			if ($line =~ /2010-$last_year  Poweradmin/) {
 				print "Updating copyright in <$fn>\n";
 				$line =~ s/2010-$last_year/2010-$curr_year/;
-				$line =~ s/\s$//;
 			}	
 			print OUT $line;
 		}
