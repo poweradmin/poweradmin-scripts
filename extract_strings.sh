ab3eaf263c01a9ac6ee6794653c9d5ad8b7224b0
Edmondas Girkantas <edmondas@girkantas.lt>
2022-12-11 16:30:57 +0200

    Cleanup script.

--- extract_strings.sh
+++ extract_strings.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-YEAR=`date "+%Y"`
-VERSION=`cat ../lib/Version.php | grep VERSION | cut -d "'" -f2`
-DATE=`date "+%Y-%m-%d %H:%M%z"`
+YEAR=$(date "+%Y")
+VERSION=$(cat ../lib/Version.php | grep VERSION | cut -d "'" -f2)
 
 cd ..
 # extract strings from php files
@@ -13,36 +12,13 @@ find . -name "*.php" | \
 		--msgid-bugs-address="edmondas@poweradmin.org" \
 		-o locale/i18n-template-php.pot \
 		--package-name=Poweradmin \
-		--package-version=$VERSION \
+		--package-version="$VERSION" \
 && sed -i -e 's/SOME DESCRIPTIVE TITLE/Poweradmin translation/' locale/i18n-template-php.pot \
 && sed -i -e 's/Language: /Language: en_EN/' locale/i18n-template-php.pot \
 && sed -i -e 's/PACKAGE/Poweradmin/' locale/i18n-template-php.pot \
-&& sed -i -e 's/(C) YEAR/(C) '$YEAR'/' locale/i18n-template-php.pot \
+&& sed -i -e 's/(C) YEAR/(C) '"$YEAR"'/' locale/i18n-template-php.pot \
 && sed -i -e 's/FIRST AUTHOR <EMAIL@ADDRESS>, YEAR/Rejo Zenger <rejo@poweradmin.org>, 2008/' locale/i18n-template-php.pot \
 && sed -i -e 's/CHARSET/UTF-8/' locale/i18n-template-php.pot
-
-# add header to db strings file
-cat >locale/i18n-template-db.pot <<EOF
-# Poweradmin translation.
-# Copyright (C) $YEAR Poweradmin Development Team
-# This file is distributed under the same license as the Poweradmin package.
-# Rejo Zenger <rejo@poweradmin.org>, 2008.
-#
-#, fuzzy
-msgid ""
-msgstr ""
-"Project-Id-Version: Poweradmin $VERSION\n"
-"Report-Msgid-Bugs-To: edmondas@poweradmin.org\n"
-"POT-Creation-Date: $DATE\n"
-"PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
-"Last-Translator: FULL NAME <EMAIL@ADDRESS>\n"
-"Language-Team: LANGUAGE <LL@li.org>\n"
-"Language: en_EN\n"
-"MIME-Version: 1.0\n"
-"Content-Type: text/plain; charset=UTF-8\n"
-"Content-Transfer-Encoding: 8bit\n"
-
-EOF
 
 # extract strings from database structure
 cat install/database-structure.inc.php | grep "array([0-9]" | \
