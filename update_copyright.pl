a5b830c35806f362dd487ebb4de757efa17d461e
Edmondas Girkantas <edmondas.girkantas@tietoevry.com>
2023-01-02 07:51:54 +0200

    Adjusted script to the current year.

--- update_copyright.pl
+++ update_copyright.pl
@@ -9,19 +9,19 @@ my @date = localtime(time);
 my $curr_year = $date[5] + 1900;
 my $last_year = $curr_year - 1;
 
-$curr_year = 2022;
-$last_year = 2017;
+$curr_year = 2023;
+$last_year = 2022;
 
 sub process_file {
 	my $fn = $File::Find::name;
-	
+
 	return if ($fn =~ /\.git/);
 
 	my $found = 0;
 
 	open(FH, "$_") or die $!." <$fn>\n";
 	while (my $line = <FH>) {
-		if ($line =~ /2010-$last_year  Poweradmin/) {
+		if ($line =~ /2010-$last_year\s+Poweradmin Development Team/) {
 			$found = 1;
 			last;
 		}
@@ -33,10 +33,10 @@ sub process_file {
 		open(OUT, "> $_.new") or die $!." <$fn.new>\n";
 
 		while (my $line = <IN>) {
-			if ($line =~ /2010-$last_year  Poweradmin/) {
+			if ($line =~ /2010-$last_year\s+Poweradmin Development Team/) {
 				print "Updating copyright in <$fn>\n";
 				$line =~ s/2010-$last_year/2010-$curr_year/;
-			}	
+			}
 			print OUT $line;
 		}
 
