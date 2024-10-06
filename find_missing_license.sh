8855f20a30b217c8f48c8b295a43be436a045e0e
Edmondas Girkantas <edmondas@girkantas.lt>
2024-10-06 18:07:42 +0300

    added script to find files without copyright

---
+++ find_missing_license.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+LIB_DIR="lib"
+
+IMPORTANT_STRING="Poweradmin, a friendly web-based admin tool for PowerDNS."
+
+find "$LIB_DIR" -type f -name "*.php" | while read -r file; do
+    # Check if the file contains the important string
+    if ! grep -qF "$IMPORTANT_STRING" "$file"; then
+        echo "File without important string: $file"
+    fi
+done
