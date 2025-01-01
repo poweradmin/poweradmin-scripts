ec377b2117ae540f6764fbce637caa20b4c771bf
Edmondas Girkantas <edmondas@girkantas.lt>
2025-01-01 18:26:39 +0200

    Improve locale message compilation script

--- update_messages.sh
+++ update_messages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-result=`msgfmt 2>/dev/null`
+msgfmt 2>/dev/null
 
 if [ "$?" = "127" ]
 then
@@ -9,16 +9,12 @@ then
 fi
 
 # get list of available locales, excluding template
-dirs=`ls ../locale | grep -v pot`
+dirs=$(find ../locale -maxdepth 1 -type d ! -name 'pot' -exec basename {} \; | tail -n +2)
 
 # update every messages.mo for every locale
 for locale in $dirs; do
 	echo "Updating $locale locale"
 
-	cd ../locale/$locale/LC_MESSAGES
-
-	msgfmt -v -C messages.po
-	msgfmt messages.po -o messages.mo
-
-	cd ../../
-done 
+  msgfmt -v -C ../locale/"$locale"/LC_MESSAGES/messages.po
+  msgfmt ../locale/"$locale"/LC_MESSAGES/messages.po -o ../locale/"$locale"/LC_MESSAGES/messages.mo
+done
