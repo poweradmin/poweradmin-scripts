08d5e67ebb1971e75837cba9fedfb18f22c6ab9a
Edmondas Girkantas <edmondas@girkantas.lt>
2025-11-22 18:22:31 +0200

    chore: improve format script reliability

--- format.sh
+++ format.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 composer install
+chmod +x vendor/bin/*
 composer format:all
 composer install --no-dev
 ./scripts/optimize-for-release.sh
+git checkout -- vendor/composer/installed.php
